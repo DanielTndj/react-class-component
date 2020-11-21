291237720b269bfd5b1141786a326766b9d4ce3b
DanielTndj <daniel.tndj@gmail.com>
2020-11-21 14:45:52 +0700

    Build component SeasonDisplay

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "Summer" : "Winter";
+  } else {
+    return latitude > 0 ? "Winter" : "Summer";
+  }
+};
+
 const SeasonDisplay = ({ latitude }) => {
-  return <div>testtt</div>;
+  const season = getSeason(latitude, new Date().getMonth());
+  const text =
+    season === "Winter" ? "Brrr, its chilly!" : "Lets hit the beach!";
+  const icon = season === "Winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`${icon} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
