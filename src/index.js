0ba558b2d762a71ddeb2b5e658a9fef3f3ca0bb0
DanielTndj <daniel.tndj@gmail.com>
2020-11-21 16:40:15 +0700

    Created renderContent class

--- src/index.js
+++ src/index.js
@@ -13,14 +13,18 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.lat && !this.state.errorMessage) {
       return <SeasonDisplay latitude={this.state.lat} />;
     } else if (!this.state.lat && this.state.errorMessage) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    return <Spinner text="Please allow us to access your current location"/>;
+    return <Spinner text="Please allow us to access your current location" />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
