c2d3fc456166899fd77107cb4c899fa62fd25ddb
DanielTndj <daniel.tndj@gmail.com>
2020-11-21 16:00:06 +0700

    Added style using semantic ui

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Brrr, it's chilly!",
+    iconName: "snowflake",
+  },
+};
 
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
-    return latitude > 0 ? "Summer" : "Winter";
+    return latitude > 0 ? "summer" : "winter";
   } else {
-    return latitude > 0 ? "Winter" : "Summer";
+    return latitude > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = ({ latitude }) => {
   const season = getSeason(latitude, new Date().getMonth());
-  const text =
-    season === "Winter" ? "Brrr, its chilly!" : "Lets hit the beach!";
-  const icon = season === "Winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${icon} icon`}></i>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-top ${iconName} icon massive`}></i>
       <h1>{text}</h1>
-      <i className={`${icon} icon`}></i>
+      <i className={`icon-bottom ${iconName} icon massive`}></i>
     </div>
   );
 };
